f617104e131e9d1553fb85d9063eb88b3a5c1d21
EmilyVanVlerah <evanvlerah@fullsail.edu>
2013-12-10 13:28:09 -0500

    Added second part of formula.

--- Week 3/Conditional Worksheet/Conditionals_Worksheet/js/script.js
+++ Week 3/Conditional Worksheet/Conditionals_Worksheet/js/script.js
@@ -42,7 +42,8 @@ var mpg = prompt("How much is your mpg?");
 var gaugeReading = prompt("What is the percentage of the fuel remaining?");
 var tankCapacity = prompt("How much gas does your car hold?");
 
-var first = (tankCapacity * gaugeReading);
+var first = tankCapacity * gaugeReading;
+var answer = first * mpg;
 
 if(answer >= 200){
 	alert("You can make it without stopping for gas!");
