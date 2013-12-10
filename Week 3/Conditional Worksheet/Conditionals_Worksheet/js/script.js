8c67ce9d33e81990cd5276d9c2ef211d97d66235
EmilyVanVlerah <evanvlerah@fullsail.edu>
2013-12-10 16:01:56 -0500

    Added prompt asking user how many people other than under 10 and seniors will attend.

--- Week 3/Conditional Worksheet/Conditionals_Worksheet/js/script.js
+++ Week 3/Conditional Worksheet/Conditionals_Worksheet/js/script.js
@@ -127,8 +127,9 @@ var specialTimeTwo = 5;
 var time = prompt("What time are you going to the movies?");
 var senior = prompt("Do you have any seniors attending (55 and older)?");
 var young = prompt("Do you have any young kids attending (Under 10)?");
+var amount = prompt("How many people will be going that isn't under 10 and not a senior?");
 
-
+if
 
 
 
