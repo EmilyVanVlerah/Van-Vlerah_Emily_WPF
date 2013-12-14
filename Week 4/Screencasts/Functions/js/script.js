6536d28a2ac1446b7dbd153dc58d6d5aaa49e591
EmilyVanVlerah <evanvlerah@fullsail.edu>
2013-12-14 14:42:48 -0500

    Added comments to the screen casts

--- Week 4/Screencasts/Functions/js/script.js
+++ Week 4/Screencasts/Functions/js/script.js
@@ -45,10 +45,10 @@ console.log(width);
 calcArea();
 
 //Functions - Parameters and Arguments
-
+//Sequence of the arguments and parameters is very specific, make sure they match up.
 calcArea(30, 20);
 
-function calcArea(w, h){
+function calcArea(w, h){//w=30, h=20
 	var area = w * h;
 	console.log(area);
 }
