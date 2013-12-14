03c3ceac511e26d9fd85bc5f1c969e0e98ea172a
EmilyVanVlerah <evanvlerah@fullsail.edu>
2013-12-14 14:23:13 -0500

    Started the first sreen cast for Functions (Basic Structure)

--- Week 4/Screencasts/Functions/js/script.js
+++ Week 4/Screencasts/Functions/js/script.js
@@ -1,9 +1,16 @@
 //Emily Van Vlerah
 //December 14th, 2013
 
+//Functions - Basic Structure
 
+function outputMsg(){
+	console. log ("Hello World!");
+}
 
-
-
-
+function calArea(){
+	var width = 20;
+	var height = 30;
+	var area = width * height;
+	console.log(area);
+}
 
