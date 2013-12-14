e1ddc94b1a4c76c1a9670163cdd32d6687a30264
EmilyVanVlerah <evanvlerah@fullsail.edu>
2013-12-14 14:31:07 -0500

    Setting up the variable scope screen cast.

--- Week 4/Screencasts/Functions/js/script.js
+++ Week 4/Screencasts/Functions/js/script.js
@@ -31,5 +31,20 @@ calcArea();
 calcArea();
 calcArea();
 
+//Functions - Variable Scope
+
+function calcArea(){
+	var width = 20;
+	var height = 30;
+	var area = width * height;
+	console.log(area);
+}
+
+calcArea();
+
+
+
+
+
 
 
