03adb1710ff5a7f2b8fdcbf47e46b854e7967d7a
EmilyVanVlerah <evanvlerah@fullsail.edu>
2013-12-14 14:52:40 -0500

    Added comments to tell where the arguments are and the where the parameters are in the function.

--- Week 4/Screencasts/Functions/js/script.js
+++ Week 4/Screencasts/Functions/js/script.js
@@ -53,14 +53,12 @@ function calcArea(w, h){//w=30, h=20
 	console.log(area);
 }
 
-
-
-function dogYears(age){
+function dogYears(age){//parameters
 	var dogYears = age * 7;
 	console.log("Sparky is" + dogYears = "years old.");
 }
 var age1 = 4
-dogYears(age1);
+dogYears(age1);//arguments
 dogYears(5);
 
 
