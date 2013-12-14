30ce635942f3a05bef2451d1680feabc1192c339
EmilyVanVlerah <evanvlerah@fullsail.edu>
2013-12-14 16:16:14 -0500

    Started the return function

--- Week 4/Functions Worksheet/Functions_Worksheet/js/script.js
+++ Week 4/Functions Worksheet/Functions_Worksheet/js/script.js
@@ -1,1 +1,22 @@
+//Emily Van Vlerah 
+//December 14th, 2013
+//Functions_Worksheet
+
+//Circumference
+//Calculate the circumference of a circle.
+
+function calcCircumference(p, r){
+	var circumference = (p * r) * 2;
+	return circumference;
+}
+
+
+
+
+
+
+
+
+
+
 
