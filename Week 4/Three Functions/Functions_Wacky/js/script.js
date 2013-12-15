1647e5a685bcd0317739250f9bc09a8be957cd25
EmilyVanVlerah <evanvlerah@fullsail.edu>
2013-12-15 16:50:09 -0500

    Added comments to tell what the calculator does and what exactly average damage output means.

--- Week 4/Three Functions/Functions_Wacky/js/script.js
+++ Week 4/Three Functions/Functions_Wacky/js/script.js
@@ -2,6 +2,9 @@
 //December 15h, 2013
 //Asignment: Functions - Functions_Wacky
 
+//This will calculate the average damage output for your weapon.
+//Meaning average amount of damage the weapon deals each turn.
+
 alert("Welcome to the Average Damage output calculator for your Weapon.")
 
 
