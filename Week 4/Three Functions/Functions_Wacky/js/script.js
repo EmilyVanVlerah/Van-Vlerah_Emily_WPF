cbe737b262b233d94099b16901dbce2dac7f5bc5
EmilyVanVlerah <evanvlerah@fullsail.edu>
2013-12-15 17:10:50 -0500

    Added prompt asking the user for the Weapon Hit Rate.

--- Week 4/Three Functions/Functions_Wacky/js/script.js
+++ Week 4/Three Functions/Functions_Wacky/js/script.js
@@ -7,9 +7,11 @@
 //The formula for this is:
 //Average Weapon Damage = Resistance * [Weapon Hit Rate * (1 - Special Rate) * (Normal Weapon Power) + (Weapon Special Hit Rate) * (Special Rate) * (Weapon Special Power)]
 
-alert("Welcome to the Average Damage output calculator for your Weapon.")
+alert("Welcome to the Average Damage output calculator for your Weapon.");
 
 var elementalResistance = 1.30;
+
+var whr = prompt("What is your Weapon Hit Rate?");
 
 
 
