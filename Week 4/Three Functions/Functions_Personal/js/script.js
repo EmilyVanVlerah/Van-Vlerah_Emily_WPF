030fe66b16efff095977a5735bff2262d97210de
EmilyVanVlerah <evanvlerah@fullsail.edu>
2013-12-15 22:04:07 -0500

    Changed the alert message.

--- Week 4/Three Functions/Functions_Personal/js/script.js
+++ Week 4/Three Functions/Functions_Personal/js/script.js
@@ -7,7 +7,7 @@
 //This is the formula:
 //HP recovered drinking one Health Potion = (50 + Level/3) to (100 + Level/3 + LUK/2 + END/2)
 
-alert("Welcome to the Health Potion Calculator.");
+alert("Welcome to the Health and Mana Potion Calculator.");
 
 var level = prompt("What is the level of your character?");
 var luck = prompt("What is your character's LUK Stat?");
@@ -27,6 +27,10 @@ function calcHeathB(l, k, e){
 	var second = (100 + l/3 + k/2 + e/2);
 	return second;
 }
+
+
+
+
 
 
 if()
