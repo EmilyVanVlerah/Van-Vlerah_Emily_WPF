ea54a0d1ab096a3ae28cd6f741e06f934a204aa2
EmilyVanVlerah <evanvlerah@fullsail.edu>
2013-12-15 23:12:40 -0500

    added more comments to the code.

--- Week 4/Screencasts/Loops/js/script.js
+++ Week 4/Screencasts/Loops/js/script.js
@@ -3,11 +3,11 @@
 
 //While Loop
 
-var b = 10;
+var b = 10;//setup the index
 
-while (b > 0){
+while (b > 0){//checks the condition
 	console.log(b + "kegs on the wall");//concatnation, which allows the while loop to countdown.
-	b--;//can incroment it by saying b++ but right not we are decromenting it.
+	b--;//can incroment it by saying b++ but right not we are decromenting it. (increments or decrements the index)
 }
 
 
